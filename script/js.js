c74b4ae4f2dae7bb1aa296d43a30c2b24c8120a5
moodyKfraa <hoodaalkfrawy321@gmail.com>
2023-04-14 15:20:56 +0200

    first commition

---
+++ script/js.js
@@ -0,0 +1,14 @@
+let name = "",
+    age = document.querySelector(".card .text #age").value,
+    country = document.querySelector(".card .text #country").value;
+
+// age.addEventListener(onblur,function(){
+//   age = document.querySelector(`.card .text #name`).value;
+// })
+
+// age.onblur() = function(){
+//   console.log(`fuck`)
+// }
+function value (){
+  name = document.querySelector(".card .text #name").value;
+}
